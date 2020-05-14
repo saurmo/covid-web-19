b55c7270ff451f2bea7bb60c124e88d0d64500bf
saurmo - Santiago Urrego Morales <santimo0108@gmail.com>
2020-05-14 06:22:58 -0500

    Creación independiente del proyecto web

---
+++ nuxt.config.js
@@ -0,0 +1,77 @@
+export default {
+  mode: "universal",
+  /*
+   ** Headers of the page
+   */
+  head: {
+    titleTemplate: "%s - " + process.env.npm_package_name,
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: ["@/plugins/vue-cookie"],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ["@nuxtjs/vuetify"],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    "@nuxtjs/axios"
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    theme: {
+      themes: {
+        dark: {
+          primary: "#1976d2",
+          accent: "#424242",
+          secondary: "#ff8f00",
+          info: "#26a69a",
+          warning: "#ffc107",
+          error: "#dd2c00",
+          success: "#00e676"
+        }
+      }
+    }
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+};
