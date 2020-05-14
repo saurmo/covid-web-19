31ebac06e58a0386108706bf62961c278f221717
saurmo - Santiago Urrego Morales <santimo0108@gmail.com>
2020-05-14 06:45:07 -0500

    actualizacion env

--- assets/config/index.js
+++ assets/config/index.js
@@ -1,5 +1,10 @@
-let url = process.env.NODE_URL || "http://localhost:3001/api/v1/";
+let url = "";
 
+if (process.env.NODE_ENV == "production") {
+  url = "https://covid-api-19.herokuapp.com/api/v1/";
+} else {
+  url = "http://localhost:3001/api/v1/";
+}
 const config = {
   url_api: url
 };
